50aaba90c2c881b09e64883b104fff58c744f4d9
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-09-04 09:05:03 +0300

    Create TheLeftovers.py

---
+++ TheLeftovers.py
@@ -0,0 +1,11 @@
+import time
+
+
+t1 = time.time()
+for _ in range(10):
+    print("Coronavirus case similar to The Leftovers scenario")
+
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
